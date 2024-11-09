50010bdce138f5caf7a84822397358274ddbb516
esdrassantos06 <esdrasirion1@gmail.com>
2024-11-09 14:27:49 +0000

    Dark button correct

--- src/components/NavbarContact.jsx
+++ src/components/NavbarContact.jsx
@@ -41,10 +41,20 @@ const NavbarContact = () => {
                                     <path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" color="orange"></path>
                                 </svg>
                                 <label className="switch text-sm font-medium cursor-pointer btn-color-mode-switch poppins-font inline-block m-0 relative" id="light">
-                                    <input value="1" className="w-12 h-6 opacity-0 absolute top-0 m-0 cursor-pointer" id="color_mode" name="color_mode" type="checkbox"  checked={darkMode} />
-
-                                    <label className={`btn-color-mode-switch-inner overflow-hidden m-0 w-36 h-8 relative block cursor-pointer before:content-['Dark'] after:content-['Light'] `} onClick={() => setDarkMode(!darkMode)} htmlFor="color_mode"></label>
-
+                                    <input
+                                        value="1"
+                                        className="w-12 h-6 opacity-0 absolute top-0 m-0 cursor-pointer"
+                                        id="color_mode"
+                                        name="color_mode"
+                                        type="checkbox"
+                                        checked={darkMode}
+                                        onChange={() => setDarkMode(!darkMode)}
+                                    />
+                                    <label
+                                        className={`btn-color-mode-switch-inner overflow-hidden m-0 w-36 h-8 relative block cursor-pointer ${darkMode ? "before:content-['Light'] after:content-['Dark']" : "before:content-['Dark'] after:content-['Light']"
+                                            }`}
+                                        htmlFor="color_mode"
+                                    ></label>
                                 </label>
 
                                 <svg viewBox="0 0 16 16" className="bi bi-moon-stars-fill ml-1 sm:ml-2" fill="currentColor" width="20" xmlns="http://www.w3.org/2000/svg">
