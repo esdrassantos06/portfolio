08ccbd7c37c4fbb674e3fc0d3db4c4633ce835fb
esdrassantos06 <esdrasirion1@gmail.com>
2025-02-22 18:52:29 +0000

    update class mockup logic

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -15,8 +15,7 @@ const ProjectGrid = () => {
     const [selectedProject, setSelectedProject] = useState(null);
 
     class Project {
-        constructor(id, title, pcMockup, mobileMockup, description, frameworks, github, deploy) {
-            this.id = id;
+        constructor(title, pcMockup, mobileMockup, description, frameworks, github, deploy) {
             this.title = title;
             this.pcMockup = pcMockup;
             this.mobileMockup = mobileMockup;
@@ -28,23 +27,23 @@ const ProjectGrid = () => {
     }
 
     const projects = [
-        new Project(1, 'Construtora Nova Horizonte', NH_Pc, NH_Phone, 'A fictional project focused on civil construction and engineering services.', 'React, Tailwind CSS, Shadcn UI', 'https://github.com/esdrassantos06/novahorizonte', 'https://novahorizonte.vercel.app/'),
+        new Project( 'Construtora Nova Horizonte', NH_Pc, NH_Phone, 'A fictional project focused on civil construction and engineering services.', 'React, Tailwind CSS, Shadcn UI', 'https://github.com/esdrassantos06/novahorizonte', 'https://novahorizonte.vercel.app/'),
 
-        new Project(2, 'Project 2', Mockup1Pc, Mockup1Phone, 'An e-commerce site built with Next.js and styled-components.', 'Next.js, styled-components', 'https://example.com/project2', 'https://deploy.com/project2'),
+        new Project( 'Project 2', Mockup1Pc, Mockup1Phone, 'An e-commerce site built with Next.js and styled-components.', 'Next.js, styled-components', 'https://example.com/project2', 'https://deploy.com/project2'),
 
-        new Project(3, 'Project 3', Mockup1Pc, Mockup1Phone, 'A custom blog template created with Gatsby and GraphQL.', 'Gatsby, GraphQL', 'https://example.com/project3', 'https://deploy.com/project3'),
+        new Project( 'Project 3', Mockup1Pc, Mockup1Phone, 'A custom blog template created with Gatsby and GraphQL.', 'Gatsby, GraphQL', 'https://example.com/project3', 'https://deploy.com/project3'),
 
-        new Project(4, 'Project 4', Mockup1Pc, Mockup1Phone, 'A custom dashboard built with Vue and Tailwind CSS.', 'Vue, Tailwind CSS', 'https://example.com/project4', 'https://deploy.com/project4'),
+        new Project( 'Project 4', Mockup1Pc, Mockup1Phone, 'A custom dashboard built with Vue and Tailwind CSS.', 'Vue, Tailwind CSS', 'https://example.com/project4', 'https://deploy.com/project4'),
 
-        new Project(5, 'Project 5', Mockup1Pc, Mockup1Phone, 'A personal finance tracker made with Angular and Firebase.', 'Angular, Firebase', 'https://example.com/project5', 'https://deploy.com/project5'),
+        new Project( 'Project 5', Mockup1Pc, Mockup1Phone, 'A personal finance tracker made with Angular and Firebase.', 'Angular, Firebase', 'https://example.com/project5', 'https://deploy.com/project5'),
 
-        new Project(6, 'Project 6', Mockup1Pc, Mockup1Phone, 'A real-time chat app built with React and Socket.io.', 'React, Socket.io', 'https://example.com/project6', 'https://deploy.com/project6'),
+        new Project( 'Project 6', Mockup1Pc, Mockup1Phone, 'A real-time chat app built with React and Socket.io.', 'React, Socket.io', 'https://example.com/project6', 'https://deploy.com/project6'),
 
-        new Project(7, 'Project 7', Mockup1Pc, Mockup1Phone, 'A task management tool created using Svelte and Tailwind CSS.', 'Svelte, Tailwind CSS', 'https://example.com/project7', 'https://deploy.com/project7'),
+        new Project( 'Project 7', Mockup1Pc, Mockup1Phone, 'A task management tool created using Svelte and Tailwind CSS.', 'Svelte, Tailwind CSS', 'https://example.com/project7', 'https://deploy.com/project7'),
 
-        new Project(8, 'Project 8', Mockup1Pc, Mockup1Phone, 'An AI-powered chatbot built with Next.js and OpenAI API.', 'Next.js, OpenAI API', 'https://example.com/project8', 'https://deploy.com/project8'),
+        new Project( 'Project 8', Mockup1Pc, Mockup1Phone, 'An AI-powered chatbot built with Next.js and OpenAI API.', 'Next.js, OpenAI API', 'https://example.com/project8', 'https://deploy.com/project8'),
 
-        new Project(9, 'Project 9', Mockup1Pc, Mockup1Phone, 'A multi-language blog developed with Astro and Markdown.', 'Astro, Markdown', 'https://example.com/project9', 'https://deploy.com/project9'),
+        new Project( 'Project 9', Mockup1Pc, Mockup1Phone, 'A multi-language blog developed with Astro and Markdown.', 'Astro, Markdown', 'https://example.com/project9', 'https://deploy.com/project9'),
     ];
 
 
@@ -62,15 +61,15 @@ const ProjectGrid = () => {
     return (
         <div className='flex flex-col items-center mx-4 mb-20'>
             <div className='grid md:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 gap-4'>
-                {visibleProjects.map((project) => (
+                {visibleProjects.map((project, i) => (
                     <div
-                        key={project.id}
+                        key={i}
                         className='relative bg-gray-200 shadow-md shadow-slate-300 active:scale-100 active:translate-y-1 hover:scale-105 dark:shadow-zinc-950 dark:bg-neutral-900 p-4 rounded-lg overflow-hidden cursor-pointer'
                         onMouseEnter={() => setHoveredProject(project.id)}
                         onMouseLeave={() => setHoveredProject(null)}
                         onClick={() => handleProjectClick(project)}
                     >
-                        <h3 className='text-black dark:text-white text-center font-semibold mb-2'>{project.title}</h3>
+                        <h3 className='text-black dark:text-white text-center font-semibold mb-5'>{project.title}</h3>
                         <img
                             src={project.pcMockup}
                             alt={`${project.title} PC Mockup`}
@@ -79,7 +78,7 @@ const ProjectGrid = () => {
                         <img
                             src={project.mobileMockup}
                             alt={`${project.title} Mobile Mockup`}
-                            className='absolute top-10 left-5 w-24 h-48 -rotate-12 object-cover rounded-lg shadow-lg'
+                            className='absolute bottom-2 left-5 w-24 h-48 -rotate-12 object-cover rounded-lg shadow-lg'
                         />
 
                         {hoveredProject === project.id && (
